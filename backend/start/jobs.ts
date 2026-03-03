17721a8cc362798a8bf50dece6076bfae6294b35
dekimiq <dekimiq@gmail.com>
2026-03-04 02:15:05 +0300

    INIT: INIT

--- backend/start/jobs.ts
+++
@@ -1,7 +0,0 @@
-// Регистрация обработчиков BullMQ-джобов.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-const jobs: Record<string, Function> = {
-  yandex_sync_job: () => import('#jobs/yandex_sync_job'),
-}
-
-export { jobs }
--- interaction/src/main.ts
+++
@@ -1,2 +0,0 @@
-console.log('Interaction service started (stub)...')
-setInterval(() => {}, 1000)
