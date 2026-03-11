73746126632d9a41ac350db3a8657de1523548e0
dekimiq <dekimiq@gmail.com>
2026-03-11 17:25:29 +0300

    updated: interface for retry config

--- packages/shared/src/types/retry.ts
+++ packages/shared/src/types/retry.ts
@@ -4,9 +4,6 @@ export interface IRetryConfig {
   retryDelaysMs: readonly number[]
   reportDelaysMs?: readonly number[]
   universalDelayMs: number
-  retryableHttpStatuses: readonly number[]
-  fatalHttpStatuses: readonly number[]
-  limitApiErrorCodes: readonly number[]
 
   extractErrorCode?: (data: any) => number | null | undefined
 }
