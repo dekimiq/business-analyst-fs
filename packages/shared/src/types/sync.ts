f480e576c69b9a62e66eb72cb9ce1310734c2310
dekimiq <dekimiq@gmail.com>
2026-03-11 13:47:12 +0300

    updated: types

--- packages/shared/src/types/sync.ts
+++ packages/shared/src/types/sync.ts
@@ -2,5 +2,5 @@ export interface ISyncService {
   readonly source: string
   sync(): Promise<void>
   isReady(): Promise<boolean>
-  getDataAvailability(): Promise<{ availableUntil: string | null }>
+  getDataAvailability(): Promise<{ availableUntil: 'none' | 'all' | string }>
 }
