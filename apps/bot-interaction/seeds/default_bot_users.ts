fc657899439e7949279b1a2eea4dd1336587b67e
dekimiq <dekimiq@gmail.com>
2026-03-19 22:55:20 +0300

    feat: added seeds for the bot

---
+++ apps/bot-interaction/seeds/default_bot_users.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex'
+import { env } from '@project/env'
+
+export async function seed(knex: Knex): Promise<void> {
+  const schema = 'bot'
+
+  const devUserId = env.USER_ID_DEV || '123456789'
+  const devUsername = env.USERNAME_DEV || 'test'
+
+  await knex.schema.withSchema(schema).raw(`
+    INSERT INTO bot.users (user_id, username, role) 
+    VALUES ('${devUserId}', '${devUsername}', 'dev') 
+    ON CONFLICT (user_id) DO NOTHING;
+  `)
+}
