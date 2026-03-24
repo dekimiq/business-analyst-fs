b462f796aad55d8869a6d6c92f621040a1a52769
dekimiq <dekimiq@gmail.com>
2026-03-24 15:56:09 +0300

    refactor: unified configurations for orm knex

--- apps/node-cron/knexfile.ts
+++ apps/node-cron/knexfile.ts
@@ -2,38 +2,28 @@ import 'dotenv/config'
 import type { Knex } from 'knex'
 
 /**
- * Поскольку мы читаем конфигурацию базы данных для knex,
- * нам нужны переменные окружения, которые лежат в корне монорепо.
+ * Унифицированный конфиг для любого окружения.
  */
-import { resolve, dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { config as dotenvConfig } from 'dotenv'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-dotenvConfig({ path: resolve(__dirname, '../../.env') })
-
-const config: { [key: string]: Knex.Config } = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST ?? 'localhost',
-      port: Number(process.env.DB_PORT ?? 5432),
-      database: process.env.DB_NAME ?? 'analytics',
-      user: process.env.DB_USER_CRON ?? 'cron_user',
-      password: process.env.DB_PASSWORD_CRON ?? 'secret_cron',
-    },
-    searchPath: ['settings', 'public'],
-    pool: { min: 2, max: 10 },
-    migrations: {
-      tableName: 'knex_migrations_cron',
-      directory: './migrations',
-      extension: 'ts',
-      loadExtensions: ['.ts'],
-    },
-    seeds: {
-      directory: './seeds',
-    },
+const config: Knex.Config = {
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST ?? 'localhost',
+    port: Number(process.env.DB_PORT ?? 5432),
+    database: process.env.DB_NAME ?? 'analytics',
+    user: process.env.DB_USER_CRON ?? 'cron_user',
+    password: process.env.DB_PASSWORD_CRON ?? 'secret_cron',
+  },
+  searchPath: ['settings', 'public'],
+  pool: {
+    min: 2,
+    max: process.env.NODE_ENV === 'production' ? 20 : 10,
+  },
+  migrations: {
+    tableName: 'knex_migrations_cron',
+    directory: './migrations',
+  },
+  seeds: {
+    directory: './seeds',
   },
 }
 
